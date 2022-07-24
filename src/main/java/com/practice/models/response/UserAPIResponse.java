70cc86f8abd4ea0969d4a9f6efa0a7147677d8d6
Marco Seman <mfseman@umich.edu>
2022-07-24 19:41:53 -0400

    Add schema response for response object

--- src/main/java/com/practice/models/response/UserAPIResponse.java
+++ src/main/java/com/practice/models/response/UserAPIResponse.java
@@ -1,5 +1,6 @@
 package com.practice.models.response;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.*;
 
 @Builder
@@ -9,7 +10,26 @@ import lombok.*;
 @NoArgsConstructor
 public class UserAPIResponse {
 
+    @Schema(
+            title = "User's ID",
+            description = "This is the users ID",
+            minimum = "0",
+            maximum = "10000",
+            example = "5")
     int id;
+
+    @Schema(
+            title = "Response Code",
+            description = "The response code returned to the user",
+            minimum = "200",
+            maximum = "504",
+            example = "0")
     int responseCode;
-    String message;
+
+    @Schema(
+            title = "Error message",
+            description = "This is an error message returned to the user",
+            pattern = "/^[a-z ,.'-]+$/i",
+            example = "Invalid Request >:(")
+    String errorMessage;
 }
